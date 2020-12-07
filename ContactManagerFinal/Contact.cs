a370c251d0b12d1c6061d5233c01d2a33bb12bca
Manh-Anh Tuan, Nguyen <Nick1234567890a@gmail.com>
2020-12-07 14:31:14 -0500

    Added "Contact" class.

---
+++ ContactManagerFinal/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagerFinal
+{
+    class Contact
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string phonenumber { get; set; }
+        public string email { get; set; }
+        public string place { get; set; }
+        public string state { get; set; }
+        public Contact() { }
+        public Contact(int id,string firstname,string lastname,string phonenumber,string email,string place,string state)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.phonenumber = phonenumber;
+            this.email = email;
+            this.place = place;
+            this.state = state;
+        }
+        
+    }
+}
