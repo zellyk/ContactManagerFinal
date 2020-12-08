a08f0ab76e3453b883255c25d282abc7571cdcb8
Olivier Lemire <olivierlemire@urbanimmersive.com>
2020-12-07 20:12:47 -0500

    First db method -> getAll

--- ContactManagerFinal/Contact.cs
+++ ContactManagerFinal/Contact.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace ContactManagerFinal
 {
-    class Contact
+    public class Contact
     {
         public int id { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
         public string phonenumber { get; set; }
+        public string username { get; set; }
         public string email { get; set; }
         public string place { get; set; }
         public string state { get; set; }
         public Contact() { }
-        public Contact(int id,string firstname,string lastname,string phonenumber,string email,string place,string state)
+        public Contact(int id,string firstname,string lastname,string phonenumber,string username,string email,string place,string state)
         {
             this.id = id;
             this.firstname = firstname;
             this.lastname = lastname;
             this.phonenumber = phonenumber;
+            this.username = username;
             this.email = email;
             this.place = place;
             this.state = state;
---
+++ ContactManagerFinal/DbUtil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagerFinal
+{
+    public sealed class DbUtil
+    {
+        DbUtil() { }
+
+        static readonly DbUtil instance = new DbUtil();
+
+        public static DbUtil Instance
+        {
+            get { return instance; }
+        }
+
+        public List<Contact> GetAll()
+        {
+            List<Contact> listContacts = new List<Contact>();
+            var query = "SELECT * People INNER JOIN Emails ON (People.Domainid = Emails.Domainid);";
+            var con = new SqlConnection(@"data source=localhost\SQLEXPRESS;database = CONTACTS;Trusted_Connection=True");
+            SqlCommand cm = new SqlCommand(query, con);
+            SqlDataReader sdr = cm.ExecuteReader();
+            while (sdr.Read())
+            {
+                Contact tempContact = new Contact(
+                    (int)sdr["Id"],
+                    (string)sdr["Firstname"],
+                    (string)sdr["Lastname"],
+                    (string)sdr["Phonenumber"],
+                    (string)sdr["Username"],
+                    (string)sdr["Name"],
+                    (string)sdr["Place"],
+                    (string)sdr["State"]);
+
+                listContacts.Add(tempContact);
+            }
+            return listContacts;
+        }
+    }
+}
